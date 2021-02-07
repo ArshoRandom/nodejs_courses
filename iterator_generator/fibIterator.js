7cbd8c8c531d8ae55a5dba9cd45f5323c632382c
ArshoRandom <papoyanarshak@gmail.com>
2021-02-07 17:05:10 +0400

    add generator

--- iterator_generator/fibIterator.js
+++ iterator_generator/fibIterator.js
@@ -1,17 +1,13 @@
 let fibonacci = {
-    [Symbol.iterator]: function (){
-
+    [Symbol.iterator]: () => {
         let current = 1;
         let previous = 0;
         let nextNum  = previous + current;
-
         return{
-            next: function (){
-
+            next: () => {
                 previous = current
                 current = nextNum
                 nextNum = previous + current;
-
                 return{
                     value: current,
                     done: false,
@@ -21,6 +17,24 @@ let fibonacci = {
     }
 }
 
+function* fib(max){
+    let current = 1;
+    let previous = 0;
+    let nextNum  = previous + current;
+    while (nextNum <= max){
+        yield nextNum
+        previous = current
+        current = nextNum
+        nextNum = previous + current;
+    }
+}
+
+console.log("With iterator\n")
+for (const fibNum of fib(100)) {
+    console.log(fibNum);
+}
+
+console.log("\nWith generator\n")
 for (const fibNum of fibonacci) {
     if (fibNum > 100) break;
     console.log(fibNum);
